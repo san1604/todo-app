a6f26f8084fea8622d174b8ee7c7f98e76dbb183
Sandeep Solanki <sandeepsolankimca@gmail.com>
2025-05-12 18:13:26 +0530

    Add to Button Added

--- todo.js
+++ todo.js
@@ -1,1 +1,22 @@
 console.log("Hello");
+
+var todoListArray = []
+
+function addToDo() {
+  console.log("Add to-do");
+  let todoInput = document.getElementById("todoInput");
+  console.log(todoInput);
+  console.log(todoInput.value);
+  if (todoInput.value.trim() === "") {
+    alert("Please enter a to-do item.");
+    
+  }
+  else {
+    console.log(todoListArray);
+    todoListArray.push(todoInput.value);
+    console.log(todoListArray);
+  }
+//   console.log(todoListArray);
+//   todoListArray.push(todoInput.value);
+//   console.log(todoListArray);
+}
