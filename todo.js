e1775e93cdc260aeae5dbf1b7b66a250b0aeb36a
Sandeep Solanki <sandeepsolankimca@gmail.com>
2025-05-13 18:03:18 +0530

    Added todos dynamically

--- todo.js
+++ todo.js
@@ -7,14 +7,23 @@ function addToDo() {
   let todoInput = document.getElementById("todoInput");
   console.log(todoInput);
   console.log(todoInput.value);
+  let ol = document.getElementById("todoList")
+  console.log(ol);
+  
+ 
   if (todoInput.value.trim() === "") {
     alert("Please enter a to-do item.");
-    
   }
   else {
     console.log(todoListArray);
     todoListArray.push(todoInput.value);
     console.log(todoListArray);
+  }
+  todoList.innerHTML = ""
+  for (let index = 0; index < todoListArray .length; index++) {
+    console.log(index);
+    ol.innerHTML += "<li> " + (todoListArray[index]) + "</li>"
+    console.log(todoList.innerHTML);
   }
 //   console.log(todoListArray);
 //   todoListArray.push(todoInput.value);
